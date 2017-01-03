6f473f927ac469e166105010b38539b33516396e
Sébastien Vercammen <contact@sebastienvercammen.be>
2017-01-03 22:36:58 +0100

    Consistency + readability fixes.

--- pogom/manual_captcha.py
+++ pogom/manual_captcha.py
@@ -10,42 +10,52 @@ log = logging.getLogger(__name__)
 
 
 def captcha_verifier(captcha_url, status):
-
     try:
         chrome_options = Options()
         chrome_options.add_argument("window-size=600,600")
         chrome_options.add_argument("window-position=500,300")
+
         driver = webdriver.Chrome(chrome_options=chrome_options)
-        # driver = webdriver.Chrome()
-        # driver.set_window_size(600, 600)
-        # driver.set_window_position(500, 300)
+
         driver.get("https://pgorelease.nianticlabs.com/")
         driver.get(captcha_url)
-        ex_script = '''window.globalVariable = "Fail";
-        var captchaPage = '<form action="?" method="POST"><div class="g-recaptcha" data-size="compact" data-sitekey="6LeeTScTAAAAADqvhqVMhPpr_vB9D364Ia-1dSgK" data-callback="captchaResponse"></form>';
-        document.body.parentElement.innerHTML = captchaPage;
+
+        ex_script = '''
+        window._pgm_captcharesponse = "Fail";
+
+        var captchaPage = '<form action="?" method="POST"><div class="g-recaptcha" data-size="compact" data-sitekey="6LeeTScTAAAAADqvhqVMhPpr_vB9D364Ia-1dSgK" data-callback="_pgm_onCaptchaResponse"></form>';
+
         var script = document.createElement('script');
-        script.src = 'https://www.google.com/recaptcha/api.js?hl=en';
         script.type = 'text/javascript';
-        document.getElementsByTagName('head')[0].appendChild(script);
+        script.src = 'https://www.google.com/recaptcha/api.js?hl=en';
+
         var script2 = document.createElement('script');
         script2.type = 'text/javascript';
-        script2.text = 'function captchaResponse(str) {window.globalVariable = str;}'
+        script2.text = 'function _pgm_onCaptchaResponse(str) {window._pgm_captcharesponse = str;}'
+
+        document.body.parentElement.innerHTML = captchaPage;
+        document.getElementsByTagName('head')[0].appendChild(script);
         document.getElementsByTagName('head')[0].appendChild(script2);
         '''
+
         driver.execute_script(ex_script)
+
         try:
-            WebDriverWait(driver, 60).until(EC.text_to_be_present_in_element_value((By.ID, "g-recaptcha-response"), ""))
-            captcha_token = driver.execute_script("return globalVariable;")
+            WebDriverWait(driver, 60).until(
+                EC.text_to_be_present_in_element_value((By.ID, 'g-recaptcha-response'), ''))
+            captcha_token = driver.execute_script(
+                'return window._pgm_captcharesponse;')
             driver.quit()
         except:
             try:
                 driver.quit()
             except:
-                status['message'] = 'Unable to close Chromedriver'
+                status['message'] = 'Unable to close ChromeDriver.'
                 log.warning(status['message'])
-            status['message'] = 'ChromeDriver Has Timed Out'
+
+            status['message'] = 'ChromeDriver has timed out.'
             log.warning(status['message'])
+
             captcha_token = 'Fail'
 
         return captcha_token
@@ -53,10 +63,12 @@ def captcha_verifier(captcha_url, status):
         try:
             driver.quit()
         except:
-            status['message'] = 'Unable to close Chromedriver'
+            status['message'] = 'Unable to close ChromeDriver.'
             log.warning(status['message'])
-        status['message'] = 'ChromeDriver was Closed, Retrying...'
+
+        status['message'] = 'ChromeDriver was closed, retrying...'
         log.warning(status['message'])
+
         captcha_token = captcha_verifier(captcha_url, status)
         return captcha_token
 
@@ -65,6 +77,7 @@ def chrome_verifier():
     try:
         driver = webdriver.Chrome()
         driver.quit()
+
         return True
     except:
         return False
