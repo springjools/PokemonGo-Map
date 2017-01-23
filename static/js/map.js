f338440f30e429321bb70d3a734bfab4295a62cc
Jools <springjools@gmail.com>
2017-01-23 22:43:56 +0200

    Fix trailing space

--- static/js/map.js
+++ static/js/map.js
@@ -723,7 +723,7 @@ function updateGymMarker(item, marker) {
     var timeDelta = (Date.now() - item['last_scanned']) / 1000 / 60 // minutes since last scan
     var opacity = (timeDelta < Store.get('obsoletion1')) ? 1.0 : (timeDelta < Store.get('obsoletion2')) ? Store.get('opacity1') : (timeDelta < Store.get('obsoletion3')) ? Store.get('opacity2') : Store.get('opacity3')
     var gymSize = 48
-    
+
     marker.setOpacity(opacity)
     marker.setIcon({url: 'static/forts/' + Store.get('gymMarkerStyle') + '/' + gymTypes[item['team_id']] + (item['team_id'] !== 0 ? '_' + getGymLevel(item['gym_points']) : '') + '.png', scaledSize: new google.maps.Size(gymSize, gymSize)})
     marker.infoWindow.setContent(gymLabel(gymTypes[item['team_id']], item['team_id'], item['gym_points'], item['latitude'], item['longitude'], item['last_scanned'], item['name'], item['pokemon'], item['gym_id']))
